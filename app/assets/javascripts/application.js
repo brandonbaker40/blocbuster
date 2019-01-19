7e4b44618f82b8513fd31f939ba0d21838079188
Brandon Baker <brandonbaker40@icloud.com>
2019-01-18 18:03:13 -0700

    loads local values into search

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -20,11 +20,42 @@
 //= require materialize-sprockets
 
 $(document).ready(function(){
+  var arrayOfIds = [];
+  var pancakes = document.getElementsByClassName('invisible_tv_show_ids');
+  for (var i = 0; i < pancakes.length; i++) {
+    arrayOfIds.push(pancakes[i].innerHTML);
+   }
+   console.log(arrayOfIds);
+
+  var arrayOfNames = [];
+  var tacos = document.getElementsByClassName('name');
+  for(var j = 0; j < tacos.length; j++) {
+    arrayOfNames.push(tacos[j].innerHTML);
+  }
+  console.log(arrayOfNames);
+
+  var keys = arrayOfNames;
+  var values = arrayOfIds;
+  var yummy = {};
+  keys.forEach((key, i) => yummy[key] = values[i]);
+
     $('input.autocomplete').autocomplete({
-      data: {
-        "The Ringer": null,
-        "Lord of the Rings": null,
-        "Bad News Bears": 'https://placehold.it/250x250'
-      },
+      data: yummy,
+      onAutocomplete: function(val) {
+          // Callback function when value is autcompleted.
+          var value = $('input.autocomplete').val();
+          console.log(yummy);
+          if(value == "Apple"){
+          		var link = window.open('http://www.apple.com', '_blank');
+          		link.location;
+          } else if(value == "Google"){
+          		var link = window.open('http://www.google.com', '_blank');
+          		link.location;
+          } else {
+          		var link = window.open('http://www.microsoft.com', '_blank');
+          		link.location;
+          }
+        },
+        limit: 10
     });
   });
