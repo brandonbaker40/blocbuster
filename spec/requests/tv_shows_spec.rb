c26a94b8566035531ca418356e358f8c792a2e94
Brandon Baker <brandonbaker40@icloud.com>
2019-01-22 13:03:52 -0700

    installs spec helpers, writes specs for controller and api requests

--- spec/requests/tv_shows_spec.rb
+++ spec/requests/tv_shows_spec.rb
@@ -2,8 +2,23 @@ require 'rails_helper'
 
 RSpec.describe "TvShows", type: :request do
   describe "GET /tv_shows" do
-    it "works! (now write some real specs)" do
+    it "shows a list of popular tv shows" do
       get tv_shows_path
+      expect(response).to have_http_status(200)
+    end
+  end
+
+  describe "GET /tv_shows/60735/view_it" do
+    it "shows the tv show profile page for 'The Flash' " do
+      get view_it_tv_show_path(60735)
+      expect(response).to have_http_status(200)
+    end
+  end
+
+  describe "GET search results on a query" do
+    it "shows a list of tv shows relevant to the search keyword" do
+      search_query = "pokemon"
+      get "/tv_shows/1548186232984/search_results?query=#{search_query}"
       expect(response).to have_http_status(200)
     end
   end
